b921ef297bc33a45519c8d8042cdf074286ab452
filippofinke <filippofinke@protonmail.ch>
2022-03-09 15:13:00 +0100

    feat: :sparkles: fetch websites data

--- server.js
+++ server.js
@@ -1,23 +1,82 @@
 const express = require("express");
+const fetch = require("node-fetch");
 const app = express();
 
 process.env.PORT = process.env.PORT || 8080;
 
 let rooms = {};
+let urlCache = {};
 
-const addMessage = (room, message) => {
-	if (!(room in rooms)) {
-		rooms[room] = [];
-	}
+const isValidHttpUrl = (string) => {
+  let url;
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;
+  }
+  return url.protocol === "http:" || url.protocol === "https:";
+};
 
-	message = {
-		id: rooms[room].length,
-		timestamp: Date.now(),
-		message,
-	};
+const fetchUrl = async (url) => {
+  if (url in urlCache) {
+    return urlCache[url];
+  }
+  let title = null;
+  let icon = null;
 
-	rooms[room].unshift(message);
-	return message;
+  try {
+    let controller = new AbortController();
+    setTimeout(() => controller.abort(), 5000);
+
+    let response = await fetch(url, {
+      signal: controller.signal,
+    });
+    let html = await response.text();
+    let titleRegex = new RegExp(/<title>(.*)<\/title>/);
+    let matches = titleRegex.exec(html);
+    if (matches && matches.length > 1) {
+      title = matches[1];
+    }
+
+    let iconRegex = new RegExp(/<link\s+rel=".*icon*."\s+href="(.*?)"+.*>/);
+    matches = iconRegex.exec(html);
+    if (matches && matches.length > 1) {
+      icon = matches[1];
+      icon = new URL(icon, url).href;
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+
+  let result = {
+    title,
+    icon,
+  };
+
+  urlCache[url] = result;
+  return result;
+};
+
+const addMessage = async (room, message) => {
+  if (!(room in rooms)) {
+    rooms[room] = [];
+  }
+
+  let result = {
+    id: rooms[room].length,
+    timestamp: Date.now(),
+    message,
+  };
+
+  if (isValidHttpUrl(message)) {
+    result = {
+      ...result,
+      ...(await fetchUrl(message)),
+    };
+  }
+
+  rooms[room].unshift(result);
+  return result;
 };
 
 app.set("trust proxy", true);
@@ -26,37 +85,37 @@ app.use(express.static("build"));
 app.use(express.json());
 
 app.get("/messages", (req, res) => {
-	let room = req.ip;
-	let messages = rooms[room] || [];
-	return res.json(messages);
+  let room = req.ip;
+  let messages = rooms[room] || [];
+  return res.json(messages);
 });
 
 app.delete("/message/:id", (req, res) => {
-	let id = req.params.id;
-	let room = req.ip;
+  let id = req.params.id;
+  let room = req.ip;
 
-	if (rooms[room]) {
-		rooms[room] = rooms[room].filter((message) => message.id != id);
-	}
+  if (rooms[room]) {
+    rooms[room] = rooms[room].filter((message) => message.id != id);
+  }
 
-	return res.sendStatus(200);
+  return res.sendStatus(200);
 });
 
 app.delete("/messages", (req, res) => {
-	let room = req.ip;
-	delete rooms[room];
-	return res.sendStatus(200);
+  let room = req.ip;
+  delete rooms[room];
+  return res.sendStatus(200);
 });
 
-app.post("/message", (req, res) => {
-	let message = req.body.message;
-	let room = req.ip;
-	if (message) {
-		return res.json(addMessage(room, message));
-	}
-	return res.sendStatus(400);
+app.post("/message", async (req, res) => {
+  let message = req.body.message;
+  let room = req.ip;
+  if (message) {
+    return res.json(await addMessage(room, message));
+  }
+  return res.sendStatus(400);
 });
 
 app.listen(process.env.PORT, () => {
-	console.log(`app listening at http://localhost:${process.env.PORT}`);
+  console.log(`app listening at http://localhost:${process.env.PORT}`);
 });
--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,24 +1,24 @@
 import "./Message.css";
 import React from "react";
 
-const Message = (props) => {
-	const handleClick = ({ target }) => {
-		if (target.nodeName === "DIV") {
-			props.onClick();
-		}
-	};
+const Message = ({ message, onDelete, onClick }) => {
+  const { message: content, timestamp, title, icon } = message;
+  const handleClick = ({ target }) => {
+    if (target.nodeName === "DIV") {
+      onClick();
+    }
+  };
 
-	return (
-		<div className="message" onClick={handleClick}>
-			{props.message.message}
-			<span className="timestamp">
-				{new Date(props.message.timestamp).toLocaleString()}
-			</span>
-			<button className="delete" onClick={props.onDelete}>
-				Delete
-			</button>
-		</div>
-	);
+  return (
+    <div className="message" onClick={handleClick} title={title} alt={title}>
+      {icon ? <img className="icon" src={icon} alt="icon" /> : null}
+      {content}
+      <span className="timestamp">{new Date(timestamp).toLocaleString()}</span>
+      <button className="delete" onClick={onDelete}>
+        Delete
+      </button>
+    </div>
+  );
 };
 
 export default Message;
